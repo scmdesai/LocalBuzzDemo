dc45edc9702c0a9ea325429a433640b6a9ace0ef
scmdesai <scmdesai@gmail.com>
2016-08-15 13:34:56 -0500

    zipcode

--- app.js
+++ app.js
@@ -64588,13 +64588,13 @@ Ext.define('Ext.direct.Manager', {
                         var storesNearBy = Ext.getStore('StoresNearby');
                         $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + latitude + "," + longitude + "&key=AIzaSyDHFtBdpwHNSJ2Pu0HpRK1ce5uHCSGHKXM", function(json1) {
                             zipcode = json1.results[0].address_components[7].short_name;
+                            var postalCode = zipcode.split("\"");
                             console.log('Zipcode is : ' + zipcode);
                             //userLocationStore.removeAt(0);
-                            console.log(latitude, longitude + ',' + zipcode);
                             userLocationStore.add({
                                 'latitude': latitude.toString(),
                                 'longitude': longitude.toString(),
-                                'zipcode': zipcode
+                                'zipcode': postalCode[1]
                             });
                             // Ext.Viewport.getActiveItem().destroy();
                             var view = Ext.Viewport.add({
