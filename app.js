9309f9f41c40730f4b6c990a2a347fc84319d890
scmdesai <scmdesai@gmail.com>
2016-08-15 23:39:07 -0500

    store filter

--- app.js
+++ app.js
@@ -64681,12 +64681,12 @@ Ext.define('Ext.direct.Manager', {
                     } else {
                         return false;
                     }
-                    store.filterBy(function(record) {
-                        return Ext.Array.indexOf(storesNearBy, record.get('customerId')) !== -1;
-                    }, this);
                 });
             });
         });
+        store.filterBy(function(record) {
+            return Ext.Array.indexOf(storesNearBy, record.get('customerId')) !== -1;
+        }, this);
     }
 }, 0, 0, [
     "component",
