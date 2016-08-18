a1b0439e30b61475c261d78c3ed4cb29d6cf760b
scmdesai <scmdesai@gmail.com>
2016-08-18 15:26:25 -0500

    send user location

--- js/index.js
+++ js/index.js
@@ -32,14 +32,7 @@ var app = {
 		 try{
 		 
 		 window.analytics.startTrackerWithId('UA-67469655-6');
-		 var userLocation = navigator.geolocation.getCurrentPosition(function(position){
-		 var latitude = position.coords.latitude;
-		 var longitude = position.coords.longitude;
-		 var postalcode;
-		 $.getJSON("http://api.geonames.org/findNearbyPostalCodesJSON?lat=" + latitude + "&lng=" + longitude + "&username=1234_5678", function(json) {
-		        postalcode = json.postalCodes[0].postalCode;
-		 });
-		 });
+		 
 		 var push = PushNotification.init({
             "android": {
                 "senderID": "226322216862"
@@ -79,9 +72,18 @@ var app = {
 					console.log(json.success + ", " + json.msg) ;
 				}
 			}
-			console.og('User Location is: ' + postalcode);
+			var userLocation = navigator.geolocation.getCurrentPosition(function(position){
+		 var latitude = position.coords.latitude;
+		 var longitude = position.coords.longitude;
+		 var postalcode;
+		 $.getJSON("http://api.geonames.org/findNearbyPostalCodesJSON?lat=" + latitude + "&lng=" + longitude + "&username=1234_5678", function(json) {
+		        postalcode = json.postalCodes[0].postalCode;
+				console.og('User Location is: ' + postalcode);
 			var data = '{"deviceType":"'+device.platform+'","registrationID":"'+data.registrationId+'","userLocation":"'+postalcode+'"}';
 			xhr.send(data);
+		 });
+		 });
+			
         });
 
         push.on('notification', function(data) {
