93b97c0570bd5a202c1ffcf27f84dff6e4e36654
scmdesai <scmdesai@gmail.com>
2016-09-16 15:19:33 -0500

    dealpicture

--- app.js
+++ app.js
@@ -65835,7 +65835,7 @@ Ext.define('Ext.direct.Manager', {
         	}
         });*/
         Ext.Viewport.getActiveItem().destroy();
-        Ext.Viewport.setActiveItem(1);
+        Ext.Viewport.setActiveItem(2);
     },
     onFavoritesActivate: function(newActiveItem, container, oldActiveItem, eOpts) {
         var store = Ext.getStore('UserPreferences');
