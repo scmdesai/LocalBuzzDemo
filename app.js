9e042e0261628a35ef40a3d2cfad20b0065acbbc
scmdesai <scmdesai@gmail.com>
2016-09-16 09:54:39 -0500

    record

--- app.js
+++ app.js
@@ -65300,7 +65300,7 @@ Ext.define('Ext.direct.Manager', {
     onInfoPainted: function(element, eOpts) {
         var record = Ext.getStore('LocalStore').getAt(0);
         console.log('Info painted');
-        console.log(record);
+        console.log('Record is : ' + record);
         if (record) {
             var name = record.get('businessName');
             var isFavorite = record.get('isFavorite');
