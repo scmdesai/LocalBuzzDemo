1a3c760bba381007a285c172f5d5e1fd31fff047
scmdesai <scmdesai@gmail.com>
2016-09-16 13:46:46 -0500

    info

--- app.js
+++ app.js
@@ -64904,20 +64904,25 @@ Ext.define('Ext.direct.Manager', {
             this.down('#dealimage').setHtml('<img src="resources/img/localbuzzicon.png" align="right" style="margin: 5px 5px 5px 5px"/><br><div style="font-size:6vw;">' + record.get('dealName') + '</div><br><br><div style="font-size:4vw;">' + record.get('dealDescription') + '</div><br><div style="font-size:3vw;">Valid ' + record.get('dealStartDate') + ' - ' + record.get('dealEndDate') + '</div>');
             this.down('#nameTxt3').hide();
         }
-        Ext.getCmp('nameTxt8').element.addListener('tap', function() {
-            var view;
-            if (Ext.Viewport.getComponent('Info')) {
-                view = Ext.Viewport.getComponent('Info');
-            } else {
-                view = Ext.Viewport.add({
-                    xtype: 'contactinfo'
-                });
-            }
-            Ext.Viewport.setActiveItem(view);
-            var storeInfo = Ext.getStore('MyJsonPStore');
-            var rec = storeInfo.findRecord('customerId', record.get('customerId'));
-            view.setRecord(rec);
-        });
+        if (Ext.Viewport.getAt(0).getItemId === 'Info') {
+            Ext.getCmp('nameTxt8').hide();
+        } else {
+            Ext.getCmp('nameTxt8').show();
+            Ext.getCmp('nameTxt8').element.addListener('tap', function() {
+                var view;
+                if (Ext.Viewport.getComponent('Info')) {
+                    view = Ext.Viewport.getComponent('Info');
+                } else {
+                    view = Ext.Viewport.add({
+                        xtype: 'contactinfo'
+                    });
+                }
+                Ext.Viewport.setActiveItem(view);
+                var storeInfo = Ext.getStore('MyJsonPStore');
+                var rec = storeInfo.findRecord('customerId', record.get('customerId'));
+                view.setRecord(rec);
+            });
+        }
     },
     setRecord: function(record) {
         (arguments.callee.$previous || Ext.Panel.prototype.setRecord).apply(this, arguments);
