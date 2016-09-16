0659774334d6775a2447ed5dfe4df42e5cd4c84f
scmdesai <scmdesai@gmail.com>
2016-09-16 01:27:24 -0500

    btn

--- app.js
+++ app.js
@@ -64878,7 +64878,7 @@ Ext.define('Ext.direct.Manager', {
                 id: 'getStoreInfoBtn',
                 itemId: 'getStoreInfoBtn',
                 top: '70%',
-                ui: 'plain'
+                ui: 'confirm'
             }
         ],
         listeners: [
@@ -64914,19 +64914,7 @@ Ext.define('Ext.direct.Manager', {
             //store.filter('businessName', businessName);
             var rec = store.findRecord('businessName', businessName);
             //var rec = store.getAt(0);
-            Ext.getCmp('getStoreInfoBtn').setHtml('About ' + businessName);
-            Ext.getCmp('getStoreInfoBtn').addListener('tap', function() {
-                var view;
-                if (Ext.Viewport.getComponent('Info')) {
-                    Ext.Viewport.getComponent('Info').destroy();
-                    console.log('Info exists');
-                }
-                view = Ext.Viewport.add({
-                    xtype: 'contactinfo'
-                });
-                view.setRecord(record);
-                Ext.Viewport.setActiveItem(view);
-            });
+            Ext.getCmp('getStoreInfoBtn').setHtml('<h4>About' + businessName + '</h4>');
         }
     }
 }, 0, [
