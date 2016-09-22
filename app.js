d1b8d1d60ae7c3741c50c97e49a036cee7687c48
scmdesai <scmdesai@gmail.com>
2016-09-22 09:45:03 -0500

    scroll

--- app.js
+++ app.js
@@ -64883,7 +64883,7 @@ Ext.define('Ext.direct.Manager', {
                 listeners: [
                     {
                         fn: function(element, eOpts) {
-                            var myScroll = new IScroll('#infoPanel', {
+                            var myScroll = new IScroll('#wrapper', {
                                     zoom: true,
                                     scrollX: true,
                                     scrollY: true,
