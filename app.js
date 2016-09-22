ca98b60e39bad6d64f108f63341ae754322f488d
scmdesai <scmdesai@gmail.com>
2016-09-22 00:44:25 -0500

    scrll

--- app.js
+++ app.js
@@ -64615,10 +64615,6 @@ Ext.define('Ext.direct.Manager', {
                 docked: 'top',
                 height: '100%',
                 layout: 'vbox',
-                scrollable: {
-                    direction: 'vertical',
-                    directionLock: true
-                },
                 items: [
                     {
                         xtype: 'button',
@@ -65606,7 +65602,8 @@ Ext.define('Ext.direct.Manager', {
     ],
     config: {
         cls: 'icon-share',
-        fullscreen: true,
+        fullscreen: false,
+        height: '100%',
         html: '',
         id: 'dealPicture',
         itemId: 'dealPicture',
@@ -65615,7 +65612,6 @@ Ext.define('Ext.direct.Manager', {
         showAnimation: 'slide',
         style: 'background:#fff',
         width: '100%',
-        scrollable: false,
         tpl: [
             '<!--<tpl if="dealImageURL">',
             '<div><img src="{dealImageURL}" style="margin: 0px 5px 0px 5px;height:250px;width:95%;border:none;"/></div>',
@@ -65931,7 +65927,7 @@ Ext.define('Ext.direct.Manager', {
                         xtype: 'component',
                         cls: 'contact-name',
                         disabled: true,
-                        height: '4vh',
+                        height: '3vh',
                         html: '<p style="font-size:3vw;text-align:center"> Published through Local Buzz',
                         id: 'nameTxt2',
                         itemId: 'nameTxt2',
