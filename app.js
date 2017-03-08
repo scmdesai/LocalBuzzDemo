32a6f892341a63cf143617ae20551d3c38dd4c6c
scmdesai <scmdesai@gmail.com>
2017-03-08 12:47:01 -0600

    aert

--- app.js
+++ app.js
@@ -64330,7 +64330,7 @@ function() {
                                         //window.open(url, '_system', 'location=yes');
                                         //console.log('Success');
                                         //Ext.Msg.alert('Suceess',null,null,null);
-                                        Ext.Msg.alert('Show this code at the register', respons);
+                                        Ext.Msg.alert('Show this code at the register', response);
                                     },
                                     failure: function(response) {
                                         //window.open(url, '_system', 'location=yes');
